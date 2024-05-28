0208df6c284f933418d4539ee580b045f44740f0
ShrinivasC96 <150146654+ShrinivasC96@users.noreply.github.com>
2024-05-28 12:11:44 +0530

    sum of even number from 1 to 50 added in main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ sum_of_odd = 0
 for i in range(1,50,2):
     sum_of_odd += i
 print("Sum of odd number from 1 to 50 is: ", sum_of_odd)    
+
+
+'''
+Program to add sum of 1 to 50 even numbers
+'''
+sum_of_even = 0
+for i in range(2,51,2):
+    sum_of_even += i
+print("Sum of even number from 1 to 50 is: ", sum_of_even)
